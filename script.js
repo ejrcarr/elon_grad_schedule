b255e1c2dc413babb79044f842df77a81589c41a
Evan Carr <ejrcarr@gmail.com>
2022-10-15 20:17:55 -0400

    Added modal to upload course data

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 
 let searchIcon = document.getElementById("search-button");
-let searchBar = document.getElementById("search-bar")
-let mainCont = document.querySelector(".main-container");
+let searchBar = document.getElementById("search-bar");
+let clearSearch = document.getElementById("clear-search");
+let addCoursesButton = document.getElementById("add-courses");
+let modal = document.getElementById("modal");
+let closeModalButton = document.getElementById("close-modal");
+let submitModal = document.getElementById("submit-button");
+let modalTextArea = document.getElementById("modal-text");
+
+let currentModalText;
+
+
 
 searchIcon.addEventListener("click", () => {
     if(searchBar.className.includes('active') && searchBar.value) {
@@ -9,6 +18,33 @@ searchIcon.addEventListener("click", () => {
     }
     else {
         searchBar.classList.toggle("active");
+        clearSearch.classList.toggle("active");
     }
-    
+});
+
+clearSearch.addEventListener("click", () => {
+    if(!(searchBar.value)) {
+        searchBar.classList.toggle("active");
+        clearSearch.classList.toggle("active");
+    }
+    else {
+        searchBar.value = "";
+    }
+});
+
+addCoursesButton.addEventListener("click", () => {
+    modal.classList.toggle("show");
+});
+
+closeModalButton.addEventListener("click", () => {
+    modal.classList.toggle("show");
+});
+
+submitModal.addEventListener("click", () => {
+    if(modalTextArea.value) {
+        modal.classList.toggle("show");
+        currentModalText = modalTextArea.value;
+        modalTextArea.value = "";
+        console.log(currentModalText);
+    }
 });
