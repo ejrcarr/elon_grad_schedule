f5c9fd442eb22486029dbe290d98da5164f50129
Evan Carr <ejrcarr@gmail.com>
2022-10-16 22:04:44 -0400

    Added generate courses to populate table

--- script.js
+++ script.js
@@ -47,44 +47,92 @@ submitModal.addEventListener("click", () => {
         modal.classList.toggle("show");
         currentModalText = modalTextArea.value;
         modalTextArea.value = "";
-        console.log(currentModalText);
     }
     
     if(currentModalText) {
+        currentModalText = currentModalText.trim();
         arrayModalText = currentModalText.split("\n");
         generateYears(arrayModalText);
-        // if(currentModalText.includes(",")) {
-
-        // }
-        // else {
-            
-        //     let courses = [];
-        //     arrayModalText = currentModalText.split("\n");
-        //     for (let entry of arrayModalText) {
-        //         if(["Wint", "Summ", "Spri", "Fall"].entry.substring(0, 5) && entry != arrayModalText[0]) {
-        //             let currentCoursesContainer = document.createElement("div");
-        //             currentCoursesContainer.appendChild(courses);
-        //             courses = [];
-        //             if (entry.substring(0, 5) === "Fall") {
-        //                 endYear = startYearentry.substring(5);
-        //             }
-        //         }
-        //         else {
-        //             courses.push(entry);
-        //         }
-        // }
-        
-        // }
-        // console.log(currentModalText.split("\n"));
+        generateCourses(arrayModalText);
     }
 });
 
+
+function generateCourses(arrayOfModalText) {
+
+    let semesters = new Set();
+    semesters.add("Fall");
+    semesters.add("Wint");
+    semesters.add("Spri");
+    semesters.add("Summ");
+
+    let semesterToCourse = new Map();
+    let currentSemester = arrayOfModalText[0];
+
+    for (let line of arrayOfModalText) {
+        let currentLine = line.substring(0,4);
+        let NEW_SEMESTER = semesters.has(currentLine);
+
+        if (NEW_SEMESTER) {
+            currentSemester = line;
+            if(!(line in semesterToCourse)) {
+                semesterToCourse.set(currentSemester, new Set());
+            }
+        }
+        else if(line) {
+           semesterToCourse.set(currentSemester, semesterToCourse.get(currentSemester).add(line));
+        }
+    }
+
+    for (let [key, value] of semesterToCourse) {
+
+        let semesterAndCourse = key.split(" ");
+        if (["Spri", "Summ", "Wint"].includes(key.substring(0,4))) {
+            semesterAndCourse[1]--;
+        }
+
+        let yearRow = document.getElementById(semesterAndCourse[1]);
+
+        let courseContainer = document.createElement("div");
+        courseContainer.classList.add("course-container");
+        courseContainer.classList.add(semesterAndCourse[0].toLowerCase());
+
+        for (let course of value) {
+            let courseDiv = document.createElement("div");
+            courseDiv.classList.add("course");
+            courseDiv.classList.add("wide-screen");
+            courseDiv.classList.add(semesterAndCourse[0]);
+            courseDiv.textContent = course;
+
+            let mobile = document.createElement("div");
+            mobile.classList.add("course");
+            mobile.classList.add(semesterAndCourse[0]);
+            mobile.classList.add("mobile");
+
+            mobile.textContent = course.substring(0, 8);
+
+            courseContainer.appendChild(courseDiv);
+            courseContainer.appendChild(mobile);
+        }
+
+        if(yearRow) {
+            yearRow.appendChild(courseContainer);
+        }
+    }
+
+}
 
 
 // TODO make so years inbetween are included, even if isnt documented
 function generateYears(arrayOfInput) {
     let defaultYears = document.querySelectorAll(".year");
     defaultYears.forEach(year => year.remove());
+
+    let semesters = new Set();
+    semesters.add("Fall");
+    semesters.add("Wint");
+    semesters.add("Spri");
+    semesters.add("Summ");
 
     const IS_COMMA_SEPARATED_INPUT = arrayOfInput[0].includes(",");
 
@@ -100,38 +148,70 @@ function generateYears(arrayOfInput) {
                 yearRow.classList.add("year");
 
                 let yearContainer = document.createElement("div");
+                yearRow.classList.add(currentYear);
+                yearRow.setAttribute('id', currentYear);
 
                 years.add(currentYear);
                 yearContainer.textContent = `${currentYear}-${parseInt(currentYear)+1}`;
 
                 yearRow.appendChild(yearContainer);
                 scheduleContainer.appendChild(yearRow);
+
+
             }
         }
     }
     else {
+        
+        let maxYear = parseInt(arrayOfInput[0].substring(arrayOfInput[0].length-4, arrayOfInput[0].length));
+        let minYear = parseInt(arrayOfInput[0].substring(arrayOfInput[0].length-4, arrayOfInput[0].length));
         for (let element of arrayOfInput) {
-            if(element.substring(0,4) === "Fall") {
+            if(semesters.has(element.substring(0,4))) {
+                let semesterAndCourse = element.split(" ");
+                
+                if(["Spri", "Summ", "Wint"].includes(semesterAndCourse[0].substring(0,4))) {
+                    semesterAndCourse[1]--;
+                }
+                else if (semesterAndCourse[0].substring(0,4) === "Fall") {
+                    semesterAndCourse[1]++;
+                }
+                maxYear = Math.max(maxYear, parseInt(semesterAndCourse[1]));
+                minYear = Math.min(minYear, parseInt(semesterAndCourse[1]));
 
+                
+                // console.log('debug')
                 //TODO MAKE THIS A SEPARATE FUNCTION
-                let yearRow = document.createElement("div");
-                yearRow.classList.add("year");
+                // let yearRow = document.createElement("div");
+                // yearRow.classList.add("year");
 
-                let yearContainer = document.createElement("div");
-                let currentYear = element.substring(5);
-                yearContainer.textContent = `${currentYear}-${parseInt(currentYear)+1}`;
+                // let yearContainer = document.createElement("div");
+                // let currentYear = element.substring(5);
+                // yearRow.classList.add(currentYear);
+                // yearRow.setAttribute('id', currentYear);
 
-                yearRow.appendChild(yearContainer);
-                scheduleContainer.appendChild(yearRow);
+                // yearContainer.textContent = `${currentYear}-${parseInt(currentYear)+1}`;
+
+                // yearRow.appendChild(yearContainer);
+                // scheduleContainer.appendChild(yearRow);
             }
         }
-    }
-}
+    
 
-function addSemesterCourses(container, arrayOfCourses) {
-    for (let course of arrayOfCourses) {
-        let div = document.createElement("div");
-        div.textContent = course;
-        container.appendChild(div);
+        for (let i = minYear; i < maxYear; i++) {
+            console.log(i)
+            let yearRow = document.createElement("div");
+            yearRow.classList.add("year");
+
+            let yearContainer = document.createElement("div");
+            yearContainer.classList.add("year-column");
+            // let currentYear = element.substring(element.length-4, element.length);
+            yearRow.classList.add(i);
+            yearRow.setAttribute('id', i);
+
+            yearContainer.textContent = `${i}-${i+1}`;
+
+            yearRow.appendChild(yearContainer);
+            scheduleContainer.appendChild(yearRow);
+        }
     }
 }
